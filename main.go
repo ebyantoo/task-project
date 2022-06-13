e4548d2972b2b65cbcce4b219945c0776036d4a0
ebyantoo <erbiantonug@gmail.com>
2022-06-13 15:22:48 +0700

    change function

--- main.go
+++ main.go
@@ -8,10 +8,10 @@ import (
 
 func main() {
 
-	cek1 := gotask.CekGanjilGenap1(19)
-	fmt.Println(cek1)
-
 	cek2 := gotask.CekGanjilGenap2(5, 8, 9)
 	fmt.Println(cek2)
+
+	cek1 := gotask.CekGanjilGenap1(19)
+	fmt.Println(cek1)
 
 }
